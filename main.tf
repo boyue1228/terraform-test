cd81901504fbf2c9d041ee4d52b56d7637927bfe
root <root@Aws-test.eurovision.net>
2022-09-27 15:08:15 +0000

    add default route table, igw and security group with ingress and egress rules

--- main.tf
+++ main.tf
@@ -3,55 +3,98 @@ provider "aws" {
 }
 
 
-variable "vpc-cidr-block" {
-    description = "my vpc cidr block"
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
-variable "subnet_cidr_block" {
-    description = "my definition of 1st subnet cidr block"
-    type = list(string)
-}
-
-variable "environment" {
-    description =  "production or dev"
-}
-
-resource "aws_vpc" "main"{
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name =  "main"
-        Environment = var.environment
+        Name = "${var.env_prefix}-vpc"
+        
     }
 }
 
-resource "aws_subnet" "main-subnet-1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.subnet_cidr_block[0]
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name = "main_sub_1"
-        Environment = var.environment
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
 
-data "aws_vpc" "existing_vpc"{
-    cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "main-subnet-2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = var.subnet_cidr_block[1]
-    availability_zone = "us-east-1b"
+/* instead of creating new route table, we're going to use default one */
+/*
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
     tags = {
-        Name = "main_sub_2"
-        Environment = "production"
+        Name = "${var.env_prefix}-igw"
     }
 }
+*/
 
-output "vpc_id" {
-    value = aws_vpc.main.id
-} 
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }    
+}
 
-output "vpc_subnet2_id" {
-    value = aws_subnet.main-subnet-2.id
-} 
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+}
+
+/* by using default route table, we don't need association anymore */
+/*
+resource "aws_route_table_association" "a-rtb-subnet" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+}
+*/
+
+resource "aws_security_group" "myapp-sg" {
+    description = "my security group"
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        description      = "TLS from myapp-VPC"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+     ingress {
+        description      = "TLS from myapp-VPC"
+        from_port        = 8080
+        to_port          = 8080
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "myapp-sg"
+  }
+}
+
+
+output "myapp-vpc-id" {
+    value = aws_vpc.myapp-vpc.id
+}
+
+output "myapp-subnet-id" {
+    value = aws_subnet.myapp-subnet-1.id
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,10 @@
-
-vpc-cidr-block = "10.0.0.0/16"
-subnet_cidr_block = ["10.0.10.0/24","10.0.20.0/24"]
-environment = "production"
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.10.0/24"
+avail_zone = "us-east-1a"
+env_prefix = "Prod"
+/*
+cidr_blocks = [ 
+    {cidr_block = "10.0.10.0/24", name = "subnet-1-cidr"},
+    {cidr_block = "10.0.20.0/24", name = "subnet-2-cidr"}
+]
+*/
